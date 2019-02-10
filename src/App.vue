8604d8222013dfc0f0d4028c6241429ef868b7a6
christofferfalkoven <christoffer.falkoven@gmail.com>
2019-02-10 21:44:18 +0100

    vuetest

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="section">
+        <div class="container">
+            <div id="app">
+                <textarea rows="20" cols="100" v-model="text"></textarea>
+                <br>
+                <text-reader @load="text = $event"></text-reader>
+                <simple-upload></simple-upload>
+                <button v-on:click="count++">You clicked me {{ count }} times.</button>
+                <button v-on:click="regEx">Convery to regEX.</button>
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SimpleUpload from './SimpleUpload.vue';
+    import TextReader from "./TextReader";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+        data: () => ({ text: "" ,regularExpression: "", count : 0}),
+        components: {SimpleUpload, TextReader},
+        methods:{
+            regEx: function () {
+
+                let reggie = new RegExp(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/mgi);
+                console.log("yo");
+                let fisfis = "201811090310".match(reggie);
+                console.log(fisfis);
+                console.log("yo2");
+                let url = this.text.toString();
+                let hej;
+                //(?:(?:.*))
+                hej = new RegExp(/(Glucose\|\d+\.\d)(?:([^:]*))(20\d{10})/mgi);
+                let heej;
+                heej = new RegExp(/((?<=Glucose\|)\d+\.\d)/mgi);
+                let yo2;
+                let heej2;
+                // ||201811090310<CR>
+                heej2 = new RegExp(/(?:|)(?:|)(20\d{10})(?=(<CR>))/mgi);
+                yo2 = url.match(heej);
+                let yo3;
+                yo3 = url.match(heej2);
+                console.log(yo3.length);
+                console.log(yo2.length);
+                console.log(yo2);
+                console.log(yo3);
+                // Glucose|6.2|mmol
+                //let hej2 = new RegExp(/20[0-9]{10}/mgi)
+                //let example = "Read on port: \\\\?\\hid#vid_1a79&pid_7410&mi_00#7&2b31b1d0&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}<STX>4R|1706|^^^Glucose|16.2|mmol/L^P||F/M0/T1||201811090310<CR><ETB>B8<CR><LF>Read on port: \\\\\\\\?\\\\hid#vid_1a79&pid_7410&mi_00#7&2b31b1d0&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}<STX>4R|1706|^^^Glucose|6.9|mmol/L^P||F/M0/T1||201711090221<CR><ETB>B8<CR><LF>\""
+                // let re = new RegExp('Glucose');
+                // this.text = this.text.match(hej);
+
+                let yo;
+                //let yo2;
+                //let test;
+                //test = hej2.exec(url)
+                // let hej3 = new RegExp(/20[0-9]{10}/mgi)
+
+                yo = url.match(hej)
+                console.log(yo)
+                let hej2;
+                hej2 = new RegExp(/20[0-9]{10}/mgi)
+                //yo2 = url.match(hej2)
+                let blood_sugar_dict = {};
+                blood_sugar_dict = yo3.reduce((obj, k, i) => ({...obj, [k]: yo2[i] }), {})
+
+
+                console.log(blood_sugar_dict)
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/SimpleUpload.vue
@@ -0,0 +1,49 @@
+<template>
+    <form @submit.prevent="sendFile" enctype="multipart/form-data">
+        <div class="field">
+            <div class="file is-boxed is primary">
+                <label class="file-label">
+                    <input
+                        type="file"
+                        ref="file"
+                        @change="selectFile"
+                        class="file-input"
+                    />
+                    <span class="file-cta">
+                        <span class="file-icon">
+                            <i class="fas fa-upload"></i>
+                        </span>
+                        <span class="file-label">
+                            Chose a file...
+                        </span>
+                    </span>
+                    <span v-if="file" class="file-name">
+                        {{file.name}}
+                    </span>
+                </label>
+            </div>
+        </div>
+        <div class="field">
+            <button class="button is-info">Submit</button>
+        </div>
+    </form>
+</template>
+<script>
+    export default {
+        name: "SimpleUpload",
+        data(){
+            return{
+                file: "",
+                text: ""
+            }
+        },
+        methods:{
+            selectFile(){
+                this.file = this.$refs.file.files[0];
+            },
+            sendFile(){
+
+            }
+        }
+    }
+</script>
